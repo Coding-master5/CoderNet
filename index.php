d1f954b47e3808f5ab500e42c2de8d7363d57701
daDude123 <153016828+Coding-master5@users.noreply.github.com>
2025-03-29 14:46:42 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,13 @@
   <head>
     <title>CoderNet</title>
     <link rel='icon' type='image/x-icon' href='/icon.png'>
+    <link rel='stylesheet' type='text/css' href='/style.css'>
   </head>
+  <body>
+    <header style='background:#444;padding:50px;'>
+      <h1>CoderNet</h1>
+      <br>
+      <p>Where your ideas become reality</p>
+    </header>
+  </body>
 </html>
